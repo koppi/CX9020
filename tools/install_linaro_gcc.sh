3093f3028f798ab6f424d4538c73827564d0ad5b
Jakob Flierl <jakob.flierl@gmail.com>
2016-06-06 04:58:46 +0200

    https => http

--- tools/install_linaro_gcc.sh
+++ tools/install_linaro_gcc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pushd tools
-wget -c https://releases.linaro.org/14.09/components/toolchain/binaries/gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux.tar.xz
+wget -c http://releases.linaro.org/14.09/components/toolchain/binaries/gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux.tar.xz
 tar xfv gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux.tar.xz
 
 # install libtool for cross compiling
